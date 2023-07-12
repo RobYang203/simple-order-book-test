a64328768ea3050187947e36b6154aca4296f04f
RobYang203 <101m06014@stud.sju.edu.tw>
2023-07-12 17:40:59 +0800

    delete unused

--- src/apis/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const server = axios.create({
-  baseURL: process.env.REACT_APP_BASENAME,
-  'Content-Type': 'application/json',
-});
-
-export const getServer = () => server;
